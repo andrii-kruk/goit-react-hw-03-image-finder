384ece26d1562d281b9d541ade2141ded4ad0787
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-07-12 13:39:44 +0200

    fixed loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,19 +1,23 @@
 import { Oval } from 'react-loader-spinner';
 
+import { LoaderWrapper } from './Loader.styled';
+
 const Loader = () => {
   return (
-    <Oval
-      height={80}
-      width={80}
-      color="#4fa94d"
-      wrapperStyle={{}}
-      wrapperClass="Loader"
-      visible={true}
-      ariaLabel="oval-loading"
-      secondaryColor="#4fa94d"
-      strokeWidth={2}
-      strokeWidthSecondary={2}
-    />
+    <LoaderWrapper>
+      <Oval
+        height={80}
+        width={80}
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClass="Loader"
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="#4fa94d"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
+      />
+    </LoaderWrapper>
   );
 };
 
